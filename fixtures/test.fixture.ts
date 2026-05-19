0faa829a13557596f69ec2208acc2b1d2eaa56a7
sanjeev339 <sanjeevkumarm@piraiinfo.com>
2026-05-19 14:57:44 +0530

    refactor: remove UserManagementTcUm001 fixtures and associated DOM reconnaissance generator

--- fixtures/test.fixture.ts
+++ fixtures/test.fixture.ts
@@ -1,18 +1,12 @@
 import { test as base } from './page.fixture';
 import { LoginAction } from '../actions/auth/LoginAction';
 import { DevDashboardAction } from '../actions/dev-dashboard/DevDashboardAction';
-import { UserManagementTcUm001Action } from '../actions/user-management-tc-um-001/UserManagementTcUm001Action';
-
 type ActionFixtures = {
-    userManagementTcUm001Action: UserManagementTcUm001Action;
     devDashboardAction: DevDashboardAction;
     loginAction: LoginAction;
 };
 
 export const test = base.extend<ActionFixtures>({
-    userManagementTcUm001Action: async ({ page }, use) => {
-        await use(new UserManagementTcUm001Action(page));
-    },
     devDashboardAction: async ({ page }, use) => {
         await use(new DevDashboardAction(page));
     },
