45b14c6abf115f4a6c7e75042f32eb1e405b9f59
adithya882590 <adithya@piraiinfo.com>
2026-05-19 11:29:46 +0530

    refactor: remove redundant user management test cases and associated page objects

--- tests/user-management-tc-um-001/user-management-tc-um-001.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '../../fixtures/test.fixture';
-import { USER_MANAGEMENT_TC_UM_001_TEST_DATA } from '../../test-data/user-management-tc-um-001/user-management-tc-um-001.data';
-
-test.describe("User Management TC-UM-001", () => {
-  test("TC-UM-001 - Verify successful manual user creation", async ({ userManagementTcUm001Action, userManagementTcUm001Page }) => {
-    await userManagementTcUm001Action.performVerifySuccessfulManualUserCreation(USER_MANAGEMENT_TC_UM_001_TEST_DATA);
-    await userManagementTcUm001Page.expectStatusResultText(String(USER_MANAGEMENT_TC_UM_001_TEST_DATA.status));
-  });
-});
